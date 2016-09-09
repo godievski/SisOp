e27ef40caac3f0797db566e04cd12a555f82cf12
godievski <die.sux@hotmail.com>
2016-09-08 22:28:40 -0500

    bug fixed

--- Lab01/ntree.c
+++ Lab01/ntree.c
@@ -6,51 +6,68 @@
 #include <sys/wait.h>
 
 int prof;
-int fd[2], padre;
+int fd[2],fd2[2], padre;
 
 void crea_procesos(int nivel);
+int calc_total(int n);
 
 int main (int argc,char* argv[]){
 	pipe(fd);
+	pipe(fd2);
 	padre = getpid();
 	if (argc > 1){
 		prof = atoi(argv[1]);
 		if(prof < 1) return 0;
-		fprintf(stderr,"%d\n",prof);
 		if(prof >= 2)
 			crea_procesos(1);			
-	}	
+	}
+	int total = calc_total(prof);
+	int foo;
+	for(int i = 1; i < total; i++)
+		read(fd2[0],&foo,sizeof(foo));
 	if(padre == getpid()){
 		char cmd[50];
 		memset(cmd,0,50);
 		sprintf(cmd,"pstree %d",padre);
 		system(cmd);
-		exit(0);
+		system("killall ntree");
 	}
 	return 0;
 }
 
 void crea_procesos(int nivel){
 	int pid = getpid();
+	write(fd2[1],&padre,sizeof(padre));
 	if (nivel >= prof){
 		read(fd[0],&padre,sizeof(padre));
 		exit(0);
-		return;
 	}
-	for(int i = 0; i < nivel+1; i++){
+	for(int i = 0; i <= nivel; i++){
 		int id;
-		if (!(id=fork())){/*child*/
+		if (!(id=fork()))/*child*/
 			crea_procesos(nivel+1);
-			break;
-		} else {
-			fprintf(stderr, "Nivel %d con pid %d padre %d, Creo proceso %d || Iter: %d\n",nivel,getpid(),getppid(), id,i+1);
+		if (id == -1){
+			fprintf(stderr,"ERROR FORK\n");
+			exit(0);
 		}
+		
 	}
-	for(int i = 0; i < nivel+1; i++){
+	for(int i = 0; i <= nivel; i++){
 		if (nivel == 1) break;
 		waitpid(-1,NULL,0);
 	}
-	if(nivel > 1)
-		read(fd[0],&padre,sizeof(padre));
+	if(nivel != 1){
+		exit(0);
+	}
+}
+
+int calc_total(int n){
+	int total = 0;
+	int prod = 1;
+	for(int i = 1; i <= n; i++){
+		prod *= i;
+		total += prod;
+	}
+	return total;
 }
 
